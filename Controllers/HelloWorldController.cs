bf625d2c13592cbb22eea21a269b85f290985943
Quan Nguyen <quannguyen128@gmail.com>
2019-02-06 11:43:03 -0600

    initial commit after completing add view

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+using System.Web;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+ using System.Text.Encodings.Web;
+// using System.Web.Mvc; 
+ 
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
